612ef8b54218bbefe4acbd3fc8c1237ff9dc4ebd
Iaio99 <giuliano.vallone@ikmail.com>
2025-05-12 18:27:47 +0200

    Updated

--- manage.py
+++ manage.py
@@ -1,18 +1,14 @@
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager, Server
+#!/usr/bin/env python3
+import os
 
-import util
-from app import app
-from models import db
+from flask_migrate import Migrate
 
-migrate = Migrate(app, app.db)
+from openctf.app import create_app
+from openctf.models import db
 
-manager = Manager(app)
-manager.add_command("db", MigrateCommand)
-
-ServerCommand = Server(host="0.0.0.0", port=80,
-                       use_debugger=True, use_reloader=True)
-manager.add_command("runserver", ServerCommand)
+app = create_app()
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
-    manager.run()
+    app.run(host="0.0.0.0", port=3000, use_debugger=True, use_reloader=True)
+
